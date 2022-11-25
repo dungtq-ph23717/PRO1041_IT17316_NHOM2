f98b48d987eb0273957f0aac2ec612fea46598e0
Nguyễn Thế Minh <118040773+minhnt2407@users.noreply.github.com>
2022-11-25 22:05:02 +0700

    SuaThemSP

--- QUANLYQUANCAFE/src/Repository/DanhMucRepository.java
+++ QUANLYQUANCAFE/src/Repository/DanhMucRepository.java
@@ -4,9 +4,9 @@
  */
 package Repository;
 
+import DomainModels.DanhMucModel;
 import Utilities.DBContext;
 import ViewModels.DanhMuc;
-import ViewModels.SanPham;
 import java.util.List;
 import java.sql.ResultSet;
 import java.sql.Connection;
@@ -21,14 +21,13 @@ public class DanhMucRepository {
 
     public List<DanhMuc> getAll() {
         String query = "SELECT [ID]\n"
-                + "      ,[MaDM]\n"
                 + "      ,[TenDM]\n"
                 + "  FROM [dbo].[DanhMuc]";
         try ( Connection con = DBContext.getConnection();  PreparedStatement ps = con.prepareStatement(query);) {
             ResultSet rs = ps.executeQuery();
             List<DanhMuc> list = new ArrayList<>();
             while (rs.next()) {
-                DanhMuc danhMuc = new DanhMuc(rs.getString(1), rs.getString(2), rs.getString(3));
+                DanhMuc danhMuc = new DanhMuc(rs.getString(1), rs.getString(2));
                 list.add(danhMuc);
             }
             return list;
@@ -38,16 +37,14 @@ public class DanhMucRepository {
         return null;
     }
 
-    public boolean add(DanhMuc danhMuc) {
+    public boolean add(DanhMucModel danhMuc) {
         String query = "INSERT INTO [dbo].[DanhMuc]\n"
-                + "           ([MaDM]\n"
-                + "           ,[TenDM])\n"
+                + "           ([TenDM])\n"
                 + "     VALUES\n"
-                + "           (?,?)";
+                + "           (?)";
         int check = 0;
         try ( Connection con = DBContext.getConnection();  PreparedStatement ps = con.prepareStatement(query);) {
-            ps.setObject(1, danhMuc.getMaDanhMuc());
-            ps.setObject(2, danhMuc.getTenDanhMuc());
+            ps.setObject(1, danhMuc.getTenDM());
             check = ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace(System.out);
@@ -55,18 +52,16 @@ public class DanhMucRepository {
         return check > 0;
     }
 
-
     public DanhMuc getOne(String ten) {
         String query = "SELECT [ID]\n"
-                + "      ,[MaDM]\n"
                 + "      ,[TenDM]\n"
-                + "  FROM [dbo].[DanhMuc]\n"
+                + "  FROM [dbo].[DanhMuc]"
                 + "  WHERE TenDM like ?";
         try ( Connection con = DBContext.getConnection();  PreparedStatement ps = con.prepareStatement(query);) {
             ps.setObject(1, ten);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                return new DanhMuc(rs.getString(1), rs.getString(2), rs.getString(3));
+                return new DanhMuc(rs.getString(1), rs.getString(2));
             }
         } catch (Exception e) {
             e.printStackTrace(System.out);
@@ -74,11 +69,11 @@ public class DanhMucRepository {
         return null;
     }
 
-//    public static void main(String[] args) {
-//        List<DanhMuc> list = new DanhMucRepository().searchTenDanhMuc("Trà sữa");
+    public static void main(String[] args) {
+//        List<DanhMuc> list = new DanhMucRepository().getAll();
 //        for (DanhMuc x : list) {
 //            System.out.println(x);
 //        }
-//    }
-
+        
+    }
 }
